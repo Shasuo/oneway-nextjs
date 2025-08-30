4f10e831921239ffc2f163391bfd520685e883e4
Admin <admin@example.com>
2025-08-30 18:22:38 +0700

    first push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-raleway",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={`${raleway.variable} font-sans`}
+      style={{ fontFamily: "var(--font-raleway)" }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
